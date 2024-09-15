4d5208bb6a2bd0be46a41bfe9fb707a02bf0d023
Rajdeep Chatterjee <rajdeep.chatterjee1902@gmail.com>
2024-09-15 19:45:13 +0530

    User Authentication

--- application/views.py
+++ application/views.py
@@ -1,5 +1,25 @@
-from flask import current_app as app
+from flask import current_app as app, jsonify
+from flask_security import auth_required, roles_required
+from .models import User, db
 
 @app.get('/')
 def home():
     return 'Hello World'
+
+@app.get('/admin')
+@auth_required("token")
+@roles_required("admin")
+def admin():
+    return "Welcome Admin"
+
+@app.get('/activate/professional/<int:pro_id>')
+@auth_required("token")
+@roles_required("admin")
+def activate_professional(pro_id):
+    professional = User.query.get(pro_id)
+    if not professional or "professional" not in professional.roles:
+        return jsonify({"message": "Professional Not Found"}), 404
+    
+    professional.active = True
+    db.session.commit()
+    return jsonify({"message": "User Activated"})
